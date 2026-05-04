6f2553b63cc28f9dbf2dc68419be37185bee9ff9
David Nest <davidcamposoliveira80@gmail.com>
2026-05-04 10:58:49 -0300

    Insert user with POST method

--- src/main/java/com/davidNest/mongoDbProject/services/UserService.java
+++ src/main/java/com/davidNest/mongoDbProject/services/UserService.java
@@ -25,7 +25,6 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
