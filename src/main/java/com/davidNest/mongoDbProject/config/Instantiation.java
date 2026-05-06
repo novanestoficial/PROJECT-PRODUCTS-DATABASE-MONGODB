c677a816f40e1766e9aed0dd64dee5af47be0385
David Nest <davidcamposoliveira80@gmail.com>
2026-05-06 11:02:12 -0300

    Projection of the author's data using DTTO.

--- src/main/java/com/davidNest/mongoDbProject/config/Instantiation.java
+++ src/main/java/com/davidNest/mongoDbProject/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.davidNest.mongoDbProject.config;
 
 import com.davidNest.mongoDbProject.domain.Post;
 import com.davidNest.mongoDbProject.domain.User;
+import com.davidNest.mongoDbProject.dto.AuthorDTO;
 import com.davidNest.mongoDbProject.repository.PostRepository;
 import com.davidNest.mongoDbProject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,13 +35,14 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 	}
 
 }
